b64872e7adbc8d4c0cb7abf0fb770916efeb39c3
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 04:45:46 -0800

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 111, 214, 5, 21 };
+            
+            int UcReqemliCem = 0;
+            int DigerCem = 0;
+            int result;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 100 && arr[1]<=999)
+                {
+                    UcReqemliCem = UcReqemliCem + arr[i];
+                }
+                else
+                {
+                    DigerCem = DigerCem + arr[i];
+                }
+            }
+            result = UcReqemliCem - DigerCem;
+            Console.WriteLine(result);
+            
+        }
+    }
+}
